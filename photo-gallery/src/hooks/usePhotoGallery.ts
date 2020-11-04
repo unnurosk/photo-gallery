16dbe63c6e6305008e9bcc0391148aed7aa9ecdf
Unnur Osk <unnur.sigurgeirsdottir@gmail.com>
2020-11-04 16:54:22 -0500

    New Functions in usePhotoGallery

--- photo-gallery/src/hooks/usePhotoGallery.ts
+++ photo-gallery/src/hooks/usePhotoGallery.ts
@@ -11,6 +11,22 @@ export function usePhotoGallery() {
     const { getPhoto } = useCamera();
     const [photos, setPhotos] = useState<Photo[]>([]);
     const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+
+    const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
+        const base64Data = await base64FromPath(photo.webPath!);
+        const savedFile = await writeFile({
+          path: fileName,
+          data: base64Data,
+          directory: FilesystemDirectory.Data
+        });
+      
+        // Use webPath to display the new image instead of base64 since it's
+        // already loaded into memory
+        return {
+          filepath: fileName,
+          webviewPath: photo.webPath
+        };
+      };
   
     const takePhoto = async () => {
       const cameraPhoto = await getPhoto({
